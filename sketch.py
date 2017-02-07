51ee3512adc4d2f1f34c65ea5612a677ec74b67c
Ben Steinberg <bsteinberg@law.harvard.edu>
2017-02-07 11:40:59 -0500

    Initial commit

---
+++ sketch.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from random import randrange
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def sketch_index():
+    """ Index route for demo sketch """
+    num = randrange(0, 10)
+    return render_template("index.html", num=num)
